f599eda63dceb0732a2c4fe3c5ef780fd66c224f
r-spell <rosanna.speller@gmail.com>
2017-10-02 17:45:55 -0500

    Failing spec

---
+++ spec/bootstrap/hello_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+require 'spec_helper'
+
+RSpec.describe 'boolean input' do
+
+  include FormtasticSpecHelper
+
+  it "runs this spec" do
+    expect(false).to be true
+  end
+end
